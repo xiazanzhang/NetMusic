d863b893be1fb4a4c9d972ca260fc31bc4f24b67
xzz <wukonglive@outlook.com>
2018-11-30 18:13:50 +0800

    v1.0.0

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,41 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Text, Audio } from '@tarojs/components'
+import './index.less'
+
+export default class Index extends Component {
+
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+    config: Config = {
+        navigationBarTitleText: '首页'
+    }
+
+    componentWillMount() { }
+
+    componentDidMount() { }
+
+    componentWillUnmount() { }
+
+    componentDidShow() { }
+
+    componentDidHide() { }
+
+    render() {
+        return (
+            <View>
+                <View className='index'>
+                    <Text>Hello world!</Text>
+                </View>
+                <View>
+                    <Audio loop src='http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46' />
+                </View>
+            </View>
+        )
+    }
+}
+
